fcdafac228d1540b55f18c1c1b81f1b5dae52033
Sumit- Patil <sp39605@gmail.com>
2022-12-30 21:57:41 +0530

    local Changes pending push

--- models.py
+++ models.py
@@ -1,36 +1,42 @@
 from database import Base
 from sqlalchemy import Column,Integer,Boolean,Text,String,ForeignKey
-from sqlalchemy_utils.types import ChoiceType
 from sqlalchemy.orm import relationship
+from sqlalchemy_utils.types import ChoiceType
+
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__='user'
     id=Column(Integer,primary_key=True)
     username=Column(String(25),unique=True)
     email=Column(String(80),unique=True)
     password=Column(Text,nullable=True)
     is_staff=Column(Boolean,default=False)
     is_active=Column(Boolean,default=False)
-    orders=relationship('Order',back_populates='user') 
+    orders=relationship('Order',back_populates='user')
 
-    #return string representaions of objects
 
-    def __repr__(self) :
+    def __repr__(self):
         return f"<User {self.username}"
-    
-class Choice(Base):
-    __tablename__='orders'
+
+
+class Order(Base):
+
     ORDER_STATUSES=(
         ('PENDING','pending'),
         ('IN-TRANSIT','in-transit'),
         ('DELIVERED','delivered')
+
     )
+
     PIZZA_SIZES=(
         ('SMALL','small'),
         ('MEDIUM','medium'),
         ('LARGE','large'),
         ('EXTRA-LARGE','extra-large')
     )
+
+
+    __tablename__='orders'
     id=Column(Integer,primary_key=True)
     quantity=Column(Integer,nullable=False)
     order_status=Column(ChoiceType(choices=ORDER_STATUSES),default="PENDING")
@@ -39,7 +45,4 @@ class Choice(Base):
     user=relationship('User',back_populates='orders')
 
     def __repr__(self):
-        return f"<Order {self.id}"
-
-
-
+        return f"<Order {self.id}>"
